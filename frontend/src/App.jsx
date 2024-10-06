e219cc7cfff95d238d5ed2ceca64e866a79e60ce
kaif-lucifer <mohdkaif5243human@gmail.com>
2024-10-07 04:09:08 +0530

    3- Auth handled and routes protected in client

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LogInPage from "./pages/auth/login/LogInPage";
@@ -6,20 +6,66 @@ import Sidebar from "./components/common/Sidebar";
 import RightPanel from "./components/common/RightPanel";
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
+import LoadingSpinner from "./components/common/LoadingSpinner";
 
 function App() {
+  const { data: authUserQuery, isLoading } = useQuery({
+    queryKey: ["authUser"], // this querykey help to fetch data without writing the below function again in other components
+    queryFn: async () => {
+      try {
+        const res = await fetch("/api/auth/me");
+        const data = await res.json();
+
+        if (data.error) return null;
+        if (!res.ok) throw new Error(data.error || "Something went wrong");
+        console.log("authUser is here:", data);
+        return data;
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    retry: false,
+  });
+
+  if (isLoading) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
   return (
     <div className="flex max-w-6xl mx-auto">
       {/* Common components, bc it's not wrapped with Routes */}
-      <Sidebar />
+      {authUserQuery && <Sidebar />}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signUp" element={<SignUpPage />} />
-        <Route path="/logIn" element={<LogInPage />} />
-        <Route path="/notifications" element={<NotificationPage />} />
-        <Route path="/profile/:username" element={<ProfilePage />} />
+        <Route
+          path="/"
+          element={authUserQuery ? <HomePage /> : <Navigate to="/logIn" />}
+        />
+        <Route
+          path="/signUp"
+          element={!authUserQuery ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/logIn"
+          element={!authUserQuery ? <LogInPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={
+            authUserQuery ? <NotificationPage /> : <Navigate to="/logIn" />
+          }
+        />
+        <Route
+          path="/profile/:username"
+          element={authUserQuery ? <ProfilePage /> : <Navigate to="/logIn" />}
+        />
       </Routes>
-      <RightPanel />
+      {authUserQuery && <RightPanel />}
+      <Toaster />
     </div>
   );
 }
--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,6 +7,8 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -16,22 +18,46 @@ const SignUpPage = () => {
     password: "",
   });
 
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: async ({ email, username, fullName, password }) => {
+      try {
+        const res = await fetch("/api/auth/signUp", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, username, fullName, password }),
+        });
+
+        const data = await res.json();
+        if (!res.ok) throw new Error(data.error || "Failed to create account!");
+
+        console.log(data);
+        return data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    onSuccess: () => {
+      toast.success("Account Created Successfully");
+    },
+  });
+
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormData({
-      email: "",
-      username: "",
-      fullName: "",
-      password: "",
-    });
-    console.log(formData);
+    e.preventDefault(); // to prevent page reload
+    mutate(formData);
+    // setFormData({
+    //   email: "",
+    //   username: "",
+    //   fullName: "",
+    //   password: "",
+    // });
   };
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
-
-  const isError = false;
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
@@ -92,9 +118,9 @@ const SignUpPage = () => {
             />
           </label>
           <button className="btn rounded-full btn-primary text-white">
-            Sign up
+            {isPending ? "Loading..." : "Sign Up"}
           </button>
-          {isError && <p className="text-red-500">Something went wrong</p>}
+          {isError && <p className="text-red-500">{error.message}</p>}
         </form>
         <div className="flex flex-col lg:w-2/3 gap-2 mt-4">
           <p className="text-white text-lg">Already have an account?</p>
